3bfc7e61cf60460228a03600713413c76a6939a4
Tommy <thomz161@gmail.com>
2023-09-11 17:54:44 +0700

    package

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"go-fundamental/calculation"
+)
+
+func main() {
+	fmt.Println("Halo, belajar Golang")
+
+	result := calculation.Add(8, 9)
+
+	fmt.Println(result)
+
+}
